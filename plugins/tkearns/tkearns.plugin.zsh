d4f584288aeeef80b27051d827f73604f44d0c76
Taylor Kearns <tkearns@gmail.com>
2013-05-15 21:40:04 -0400

    Migrate commands

--- plugins/tkearns/tkearns.plugin.zsh
+++ plugins/tkearns/tkearns.plugin.zsh
@@ -1,6 +1,9 @@
 alias c='clear'
 alias lsl='ls -al'
 alias be='bundle exec'
-alias migrate='bundle exec rake db:migrate && bundle exec rake db:test:prepare'
+alias migrate='bundle exec rake db:migrate db:test:prepare'
+alias zeusmigrate='zeus bundle exec rake db:migrate db:test:prepare'
+alias rss='rspec spec/'
+alias zrss='zeus rspec spec/'
 
 function port() {lsof -i tcp:"$1";}
