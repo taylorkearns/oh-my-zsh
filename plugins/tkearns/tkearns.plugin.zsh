cc5ed7f6ae1987eeee113f2a92588f852cb2f0f5
Taylor Kearns <tkearns@gmail.com>
2014-07-30 09:36:09 -0400

    cleanlocal() for cleaning merged git branches

--- plugins/tkearns/tkearns.plugin.zsh
+++ plugins/tkearns/tkearns.plugin.zsh
@@ -7,10 +7,20 @@ alias migrate='bundle exec rake db:migrate db:test:prepare'
 alias zmigrate='zeus rake db:migrate db:test:prepare'
 alias zrss='zeus rspec spec/'
 
-function port() {lsof -i tcp:"$1";}
+function port() { lsof -i tcp:"$1"; }
+
+function cleanlocal() {
+  echo "Before cleanup:";
+  git branch;
+  echo "----";
+  git branch --merged | grep -v "\*" | xargs -n 1 git branch -D;
+  echo "----";
+  echo "After cleanup:";
+  git branch;
+}
 
 function removeextras() {
-  rm spec/controllers/$1_controller_spec.rb &&
-  rm app/helpers/$1_helper.rb &&
-  rm spec/helpers/$1_helper_spec.rb &&;
+  rm spec/controllers/$1_controller_spec.rb;
+  rm app/helpers/$1_helper.rb;
+  rm spec/helpers/$1_helper_spec.rb;
 }
