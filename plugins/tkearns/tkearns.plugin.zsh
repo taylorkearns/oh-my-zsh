4b3680fe28d599a081d395774fe4e45834bedf35
Taylor Kearns <tkearns@gmail.com>
2016-06-20 12:05:33 -0400

    Remove unused functionality from zsh

--- plugins/tkearns/tkearns.plugin.zsh
+++ plugins/tkearns/tkearns.plugin.zsh
@@ -9,38 +9,8 @@ alias migrate="bundle exec rake db:migrate db:test:prepare"
 
 function port() { lsof -i tcp:"$1"; }
 
-function cleanlocal() {
-  echo "Before cleanup:";
-  git branch;
-  echo "----";
-  git branch --merged | grep -v "\*" | xargs -n 1 git branch -D;
-  echo "----";
-  echo "After cleanup:";
-  git branch;
-}
-
-function mergeFirstIntoSecond() {
-  git checkout $1;
-  git branch --set-upstream-to=origin/$1 $1
-  git pull --rebase;
-  git checkout $2;
-  git branch --set-upstream-to=origin/$2 $2
-  git pull --rebase;
-  git merge $1;
-}
-
 function track() {
   branch_name=$(git branch | grep "*");
   branch_name="${branch_name/\* /}";
   git branch --set-upstream-to=origin/$branch_name $branch_name;
-}
-
-# protractor integration specs for Plan
-function protract() {
-  if [ $# -eq 1 ]
-  then
-    node_modules/protractor/bin/protractor protractor/conf.js --specs=protractor/features/$1.feature
-  else
-    node_modules/protractor/bin/protractor protractor/conf.js
-  fi
 }
